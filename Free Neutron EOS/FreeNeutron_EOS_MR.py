4b15f06e8a3d76fe14e70e08ebc669d0efa0c287
samiul-bashir <samiul23@iiserb.ac.in>
2025-12-23 16:03:40 +0530

    Update FreeNeutron_EOS_MR.py

--- Free Neutron EOS/FreeNeutron_EOS_MR.py
+++ Free Neutron EOS/FreeNeutron_EOS_MR.py
@@ -27,3 +27,39 @@ ms = 500.0/hbarc     # meson mass (fm-1)
 mw=783/hbarc         # some mass in (fm-1) i forogot 
 M  = 939.0/hbarc     # nucleon mass (fm-1)
 y  = 2.0           
+#%%
+number_density =np.linspace(0.08, 2, 15) #fm^-3
+M=939 #MeV
+#%%
+def integrand(k, M):
+    return np.sqrt(M**2 + k**2) * k**2      # k in MeV
+#%%
+energy_densities = []
+pressures = []
+mu_list=[]
+
+for n in number_density:
+    # Computing Fermi momentum k_F in fm^-1
+    kF = ((3 * np.pi**2 * n)**(1/3))
+    #and then converting into MeV
+    kF=kF*hbarc
+    # calculating the chemical potential mu (in MeV)
+    mu=(M**2 + kF**2)**(1/2)
+    mu_list.append(mu)
+
+# Performing the  integration
+    I, err = quad(integrand, 0, kF, args=(M,))
+    epsilon = (1 / np.pi**2) * I # in MeV^4
+    # Converting into MeV/fm^3
+    epsilon=epsilon/(hbarc**3)
+    energy_densities.append(epsilon)
+
+    #Calculating pressure p:
+    #(converting n to MeV^3) n [fm^-3] * (hbarc)^3 → MeV^3
+    p = mu *n  - epsilon
+    pressures.append(p)
+
+
+# Convert to numpy array
+energy_densities = np.array(energy_densities)
+pressures = np.array(pressures)
